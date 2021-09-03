faaffdab63eb108a9de8aa9ee34313e56f974683
ddorn <diego.dorn@free.fr>
2021-09-03 12:24:20 +0200

    put tictactoe on a canvas

--- frontend/src/components/languages/TicTacToe/TicTacToe.vue
+++ frontend/src/components/languages/TicTacToe/TicTacToe.vue
@@ -1,24 +1,15 @@
 <template>
-  <table>
-    <tr v-for="(row, r) in grid" :key="row">
-      <td v-for="(play, c) in row" :key="play" class="border text-center" :class="{'bg-blue-300': is_highlighted(r, c)}">
-        <div v-if="play !== '.'" class="w-5 h-5">
-          {{ play }}
-        </div>
-        <div v-else class="w-5 h-5"></div>
-      </td>
-    </tr>
-  </table>
+  <canvas id="tictactoe" :width="size" :height="size"></canvas>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, watchEffect, PropType } from "vue";
 
 export default defineComponent({
   name: "TicTacToe",
   props: {
     grid: {
-      type: Array,
+      type: Array as PropType<string[][]>,
       required: true,
       validator(value: Array<Array<string>>): boolean {
         if (value.length != 3) {
@@ -43,30 +34,114 @@ export default defineComponent({
         return /^[/\\.]|[-|][123]$/.test(value);
       },
     },
+    size: {
+      type: Number,
+      default: 100,
+    },
   },
   methods: {
-    is_highlighted(row: number, col: number): boolean {
-      if (!this.highlight) {
-        return false;
+    caseCenter(x: number, y: number) {
+      return [
+        (x + 0.5) * this.$el.width / 3,
+        (y + 0.5) * this.$el.height / 3
+      ];
+    },
+    redraw(): void {
+      const ctx = this.$el.getContext("2d");
+      if (!ctx) return;
+
+      const w = this.$el.width,
+        h = this.$el.height;
+
+      // Draw the grid
+      ctx.beginPath();
+      ctx.strokeStyle = '#888'
+      for (const i of [1, 2]) {
+        ctx.moveTo(w * i / 3, 0);
+        ctx.lineTo(w * i / 3, h);
+        ctx.moveTo(0, h * i / 3);
+        ctx.lineTo(w, h * i /  3);
       }
-      if (this.highlight === ".") {
-        return true;
-      }
-      if (this.highlight === "\\") {
-        return row == col;
-      }
-      if (this.highlight === "/") {
-        return row + col == 2;
-      }
-      if (this.highlight[0] === "|") {
-        return (col + 1).toString() === this.highlight[1];
-      }
-      if (this.highlight[0] === "-") {
-        return (row + 1).toString() === this.highlight[1];
+      ctx.stroke();
+      ctx.strokeStyle = 'black'
+
+      // Draw Xs and Os
+      for (const x of [0, 1, 2]) {
+        for (const y of [0, 1, 2]) {
+          const c = this.grid[y][x];
+          const [center_x, center_y] = this.caseCenter(x, y);
+          ctx.beginPath();
+          ctx.lineWidth = 2;
+          if (c == "O") {
+            ctx.arc(center_x, center_y, Math.min(w, h) / 12, 0, Math.PI * 2);
+          } else if (c == "X") {
+            ctx.moveTo(center_x - w / 12, center_y - h / 12);
+            ctx.lineTo(center_x + w / 12, center_y + h / 12);
+            ctx.moveTo(center_x - w / 12, center_y + h / 12);
+            ctx.lineTo(center_x + w / 12, center_y - h / 12);
+          }
+          ctx.stroke();
+        }
       }
 
-      return true;
+      // Draw win line
+      if (!this.highlight) {
+        return;
+      }
+      if (this.highlight === ".") {
+        // TODO: How to visualise a draw ?
+      }
+      ctx.beginPath();
+      ctx.strokeStyle = "red";
+      ctx.lineCap = "round";
+      ctx.lineWidth = 6;
+      if (this.highlight === "\\") {
+        ctx.moveTo(...this.caseCenter(0, 0));
+        ctx.lineTo(...this.caseCenter(2, 2));
+      } else if (this.highlight === "/") {
+        ctx.moveTo(...this.caseCenter(2, 0));
+        ctx.lineTo(...this.caseCenter(0, 2));
+      } else if (this.highlight[0] === "|") {
+        const column = parseInt(this.highlight[1]) - 1;
+        ctx.moveTo(...this.caseCenter(column, 0));
+        ctx.lineTo(...this.caseCenter(column, 2));
+      } else if (this.highlight[0] === "-") {
+        const row = parseInt(this.highlight[1]) - 1;
+        ctx.moveTo(...this.caseCenter(0, row));
+        ctx.lineTo(...this.caseCenter(2, row));
+      }
+      ctx.stroke();
+
     },
+    // is_highlighted(row: number, col: number): boolean {
+    //   if (!this.highlight) {
+    //     return false;
+    //   }
+    //   if (this.highlight === ".") {
+    //     return true;
+    //   }
+    //   if (this.highlight === "\\") {
+    //     return row == col;
+    //   }
+    //   if (this.highlight === "/") {
+    //     return row + col == 2;
+    //   }
+    //   if (this.highlight[0] === "|") {
+    //     return (col + 1).toString() === this.highlight[1];
+    //   }
+    //   if (this.highlight[0] === "-") {
+    //     return (row + 1).toString() === this.highlight[1];
+    //   }
+    //
+    //   return true;
+    // },
+  },
+  mounted() {
+    // console.log(this.$el)
+    // this.ctx = this.$el.getContext("2d");
+    watchEffect(() => {
+      this.redraw();
+    });
   },
 });
 </script>
