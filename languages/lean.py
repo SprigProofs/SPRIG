773955347083c261e02e48e2930c3b9fdcade3cd
ddorn <diego.dorn@free.fr>
2022-04-24 13:14:44 +0200

    lib: remove assert False

--- languages/lean.py
+++ languages/lean.py
@@ -38,15 +38,15 @@ class Lean(Language):
         return lean_validate('\n'.join(machine_proof)) == 0
 
     def validate_subclaims(self, sprig, root_statement: str, common_proof_part: List[str], *sub_claim_statements: str):
-        if extract_lemma_name(sub_claim_statements[-1]) != extract_lemma_name(root_statement):
-            assert False
+        assert extract_lemma_name(sub_claim_statements[-1]) == extract_lemma_name(root_statement)
 
         common_code = '\n'.join(common_proof_part)
         for statement in sub_claim_statements:
-            if lean_validate(common_code + '\n' + statement) != 0:
-                assert False
+            assert lean_validate(common_code + '\n' + statement) != 0
 
         return True
 
     def validate_top_level(self, sprig, initial_statement: str):
-        return extract_lemma_name(initial_statement) is not None
+        assert extract_lemma_name(initial_statement) is not None
+
+        return True
