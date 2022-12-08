d8b7e9758c9118f41cc8bc8501eddc5321c8d76f
Gabin Kolly <gabin1999@hotmail.com>
2022-12-08 15:22:23 +0100

    Added comments to index.mjs

--- reach/index.mjs
+++ reach/index.mjs
@@ -11,15 +11,24 @@ export const deadlineFromTime = (time) => time + Date.now();
 
 const sha256 = (text) => stdlib.digest(stdlib.T_Bytes(100_000_000), text);
 
-export const hashingAnswer = (proof, addressContractParent="") => 
+export const hashingAnswer = (proof, addressContractParent="") =>
+  /*
+    Returns the hash of an answer. The hash is in hexadecimal,
+    proof and addressContractParent are strings.
+  */
   sha256(proof + addressContractParent);
 
+// Takes an array of ints between 0 and 16^2 and return the concatenation of them in hexadecimal
 const uIntArrayToHex = (a) => "0x" + Array.from(a)
                                 .map(x => x.toString(16))
-                                .map(x => x.length == 1 ? "0" + x : x)
+                                .map(x => x.length == 1 ? "0" + x : x) // So that each int becomes a two character hexadecimal number
                                 .join('');
 
 export const hashingChallenge = (addressContractAnswer, indexPartChallenged) =>
+  /*
+    Returns the hash of a challenge. The hash is in hexadecimal,
+    addressContractAnswer is a string and indexPartChallenged is an int.
+  */
   sha256(addressContractAnswer + indexPartChallenged.toString());
 
 export const answer = async (account,
@@ -34,6 +43,15 @@ export const answer = async (account,
   /*
     Create and return a new contract for a new Sprig or
     to answer a challenge.
+    addressSkeptic should be the address of the author of
+    the challenge to which this answer answers, if it exists,
+    and otherwise null.
+    interactionHash should be the hash of the answer.
+    deadline should be the UNIX timestamp in milliseconds.
+    wagerUp and wagerDown should be bigNumbers.
+    isBottom is a boolean describing if the answer is a leaf
+    in the sprig tree, or not, so if it is a formal proof
+    or not.
   */
   const ctc = account.contract(backendClaim);
   const interact = {
@@ -74,7 +92,6 @@ export const challenge = async (account,
   /*
     Create and return the contract for a new
     challenge.
-
   */
   const ctc = account.contract(backendChallenge);
   const interact = {
@@ -99,8 +116,9 @@ const verifyAnswer = async (ctc,
                           ) => {
   /*
     To verify the answer contract given by the user. Returns a boolean.
-    A view is a function that returns a Maybe, because it can be not set for the moment.
+    It verifies that the contract has the same parameters as the ones given.
   */
+  // A view returns a maybe, that's why we need to put [1] everywhere.
   const correctAuthor = (await ctc.views.author())[1] == author;
   const correctSprig = (await ctc.views.addressSprig())[1] == addressSprig;
   const correctSkeptic = (await ctc.views.addressSkeptic())[1][1] ==  addressSkeptic;
@@ -122,6 +140,11 @@ const verifyChallenge = async (ctc,
                                deadline,
                                wagerDown,
                                ) => {
+  /*
+    To verify the challenge contract given by the user. Returns a boolean.
+    It verifies that the contract has the same parameters as the ones given.
+  */
+  // A view returns a maybe, that's why we need to put [1] everywhere.
   const correctAuthor = (await ctc.views.author())[1] == author;
   const correctSprig = (await ctc.views.addressSprig())[1] == addressSprig;
   const correctInteraction = uIntArrayToHex((await ctc.views.interaction())[1]) == interactionHash;
@@ -135,10 +158,11 @@ export const monitorEventsAnswer = async (ctc) => {
     Run to have messages on the console for each
     important event of the contract.
   */
-  ctc.events.incorrectContract.monitor((event) => {console.log(`The contract was deemed incorrect at time ${event.when}.`)})
-  ctc.events.newParticipant.monitor((event) => {console.log(`A new participant has been added, with address ${event.what[0]}, with contract ${event.what[1]}`)});
-  ctc.events.announceWinner.monitor((event) => {console.log(`The winner is announced: the claim was ${event.what[0] ? "correct" : "incorrect"} and the winner is ${event.what[1]} with contract ${event.what[2]}`)});
-  ctc.events.announceVerification.monitor((event) => {console.log(`The verification is done, and the claim was ${event.what[0] ? "correct" : "incorrect"}`)});
+  const address = await ctc.getAddress();
+  ctc.events.incorrectContract.monitor((event) => {console.log(`The contract ${address} was deemed incorrect at time ${event.when}.`)})
+  ctc.events.newParticipant.monitor((event) => {console.log(`A new participant has been added to the contract ${address}, with address ${event.what[0]}, with contract ${event.what[1]}`)});
+  ctc.events.announceWinner.monitor((event) => {console.log(`The winner is announced for the contract ${address}: the claim was ${event.what[0] ? "correct" : `incorrect and the winner is ${event.what[1]} with contract ${event.what[2]}`}`)});
+  ctc.events.announceVerification.monitor((event) => {console.log(`The verification is done for the contract ${address}, and the claim was ${event.what[0] ? "correct" : "incorrect"}`)});
 }
 
 export const monitorEventsChallenge = async (ctc) => {
@@ -146,21 +170,19 @@ export const monitorEventsChallenge = async (ctc) => {
     Run to have messages on the console for each
     important event of the contract.
   */
-  ctc.events.incorrectContract.monitor((event) => {console.log(`The contract was deemed incorrect at time ${event.when}.`)})
-  ctc.events.newParticipant.monitor((event) => {console.log(`A new participant has been added, with address ${event.what[0]}, with contract ${event.what[1]}`)});
-  ctc.events.announceWinner.monitor((event) => {console.log(`The winner is announced: the claim was ${event.what[0] ? "correct" : "incorrect"} and the winner is ${event.what[1]} with contract ${event.what[2]}`)});
+  ctc.events.incorrectContract.monitor((event) => {console.log(`The contract ${address} was deemed incorrect at time ${event.when}.`)})
+  ctc.events.newParticipant.monitor((event) => {console.log(`A new participant has been added to the contract ${address}, with address ${event.what[0]}, with contract ${event.what[1]}`)});
+  ctc.events.announceWinner.monitor((event) => {console.log(`The winner is announced for the contract ${address}: the claim was ${event.what[0] ? "correct" : `incorrect and the winner is ${event.what[1]} with contract ${event.what[2]}`}`)});
 }
 
 const getParticipants = async (ctc) => {
   /*
     Return the list of participants. More precisely, it
     returns the list of pairs (addressAccount, addressContract).
-    If there is no participants, it returns None.
-
   */
   const resultView = await ctc.views.participants()
   if (resultView[0] == "None"){
-    return None
+    return [];
   }
   else{
     // resultView[1] is of the form [["Some", address0], ["Some", address1],..., ["Some", addressn], ["None", Null], ["None", Null],..., ["None", Null]]
@@ -169,24 +191,39 @@ const getParticipants = async (ctc) => {
   }
 }
 
-const announceIsCorrect = (ctc) => {
-  ctc.apis.Sprig.announceWinner(true, 0);
-}
-
-const announceVerification = (ctc, verification) => {
-  ctc.apis.Sprig.announceVerification(verification);
-}
-
-const announceWinner = (ctc, index) => {
-  ctc.apis.Sprig.announceWinner(false, index);
-}
-
 export const getBalance = async (acc) => {
   // To get the balance of an account
   const bal = await acc.balanceOf();
   return `${stdlib.formatCurrency(bal, 4)} ${stdlib.standardUnit}`;
 };
 
+/* When called with parameters from a command line
+  To verify a claim:
+    ["VERIFY", "ANSWER", addressContract,
+    author, addressSkeptic, deadline,
+    wagerUp, wagerDown, hashInteraction,
+    isBottom]
+
+  To verify a challenge:
+    ["VERIFY", "CHALLENGE", addressContract,
+    author, "None", deadline,
+    0, wagerDown, hashInteraction,
+    "False"]
+
+  To add a participant:
+    ["ADD_PARTICIPANT", "ANSWER" || "CHALLENGE",
+    addressContract, addressNewParticipant,
+    adressContractNewParticipant]
+
+  To announce a verification:
+    ["ANNOUNCE_VERIFICATION", "ANSWER", addressContract,
+    wasRight]
+    
+  To announce a winner:
+    ["ANNOUNCE_WINNER", "ANSWER" || "CHALLENGE",
+    wasRight, addressContractWinner]
+  if wasRight = true, you can put addressContractWinner = 0
+*/
 if (process.argv.length > 2){
   const [action, typeContract, addressContract] = process.argv.slice(2,5);
   const backend = {"CHALLENGE":backendChallenge, "ANSWER":backendClaim}[typeContract];
