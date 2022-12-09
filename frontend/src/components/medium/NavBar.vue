f57933d69ea2b46d65adc4067823520e89f14a50
ddorn <diego.dorn@free.fr>
2022-12-09 17:39:30 +0100

    web: fix connection

--- frontend/src/components/medium/NavBar.vue
+++ frontend/src/components/medium/NavBar.vue
@@ -5,7 +5,7 @@
                 <li><router-link to="/">Home</router-link></li>
                 <li><router-link to="/search">Explore</router-link></li>
                 <li><router-link to="/new">New</router-link></li>
-                <li><router-link :to="{ name: 'user', params: { name: store.user || 'Diego' }}">Contributions</router-link></li>
+                <li v-if="store.account?.address"><router-link :to="{ name: 'user', params: { name: store.account.address }}">Contributions</router-link></li>
                 <li><router-link to="/docs">Docs</router-link></li>
                 <li class="flex-grow text-right">
                     <Button v-if="!store.account"
