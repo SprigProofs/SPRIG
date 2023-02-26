5039cfd0851f8581a7337157b3382f96c69806c5
ddorn <diego.dorn@free.fr>
2023-02-26 12:05:16 +0100

    add script to periodically upadte all servers

---
+++ periodic-ping.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# request /everything every ~5 minutes for the three sprig servers
+# so that they update their instances
+while true; do
+    date
+    printf "Origin\n"
+    curl -s http://localhost:8601/everything
+    printf "\nViazovska\n"
+    curl -s http://localhost:8602/everything
+    printf "\nGrothendieck\n"
+    curl -s http://localhost:8603/everything
+    echo
+    sleep 300
+done
